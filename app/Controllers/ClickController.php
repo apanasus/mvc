df9552816593f4496b5201530997afe6515c3c6c
apanaSUS <apanasHD2@gmail.com>
2024-06-09 20:38:04 +0300

    чистим чпу

--- app/Controllers/ClickController.php
+++ app/Controllers/ClickController.php
@@ -3,14 +3,11 @@ namespace App\Controllers;
 
 use Core\Controller;
 
-class ClickController extends Controller{
-    
-    public function index(){
-        $data = ['Click' =>[
-            'Click',
-            'Click',
-            'Click'
-        ]];
-        $this->view('Click',$data);
+class ClickController extends Controller
+{
+    public function index()
+    {
+        $data = ['message' => 'This is the Click page'];
+        $this->view('click', $data);
     }
 }
--- core/Router.php
+++ core/Router.php
@@ -1,31 +1,31 @@
-<?
+<?php
+
 namespace Core;
 
-class Router{
-    
-    public function run(){
+class Router
+{
+    public function run()
+    {
         $controllerName = 'Home';
-        $actionName ='index';
+        $actionName = 'index';
 
         $uri = trim($_SERVER['REQUEST_URI'], '/');
 
-        if($uri){
+        if ($uri) {
             $segments = explode('/', $uri);
             $controllerName = ucfirst(array_shift($segments));
-            $actionName = array_shift($segments);
+            $actionName = array_shift($segments) ?: 'index'; // Default to 'index' if no action is specified
         }
 
         $controllerClass = "\\App\\Controllers\\{$controllerName}Controller";
-        
-        if(class_exists($controllerClass)){
+        if (class_exists($controllerClass)) {
             $controller = new $controllerClass();
-            if(method_exists($controller, $actionName)){
+            if (method_exists($controller, $actionName)) {
                 call_user_func_array([$controller, $actionName], []);
-            }
-            else{
+            } else {
                 echo "Action {$actionName} not found";
             }
-        }else{
+        } else {
             echo "Controller {$controllerName} not found";
         }
     }
